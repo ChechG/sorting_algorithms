9f34e6a69a44fd06ecaa4a095615489561eb0d56
ChechG <2100@holbertonschool.com>
2021-01-31 22:01:08 +0000

    100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,17 +9,17 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t n = 0, i, j;
-	int temp;
+	int temp = 0;
 
 	if (size < 2)
 		return;
 	while (n <= size)
-		n = n * 3 + 1;
+		n = (n * 3) + 1;
 	while (n > 1)
 	{
+		n = (n - 1) / 3;
 		for (i = 0; i < (size - n); i++)
 		{
-			n = (n - 1) / 3;
 			for (j = i + n; j > 0 && j >= n; j = j - n)
 			{
 				if (array[j] < array[j - n])
