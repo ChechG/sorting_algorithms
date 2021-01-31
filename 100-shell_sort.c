bb5451a891ec71b889305fcb6020935893ee8e08
ChechG <2100@holbertonschool.com>
2021-01-31 18:51:06 +0000

    100

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * shell_sort -  sorts an array of integers in ascending order
+ * @array: array to sort
+ * @size: size of array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+    size_t n = 0, i;
+    int temp, j;
+
+    while (n <= size)
+        n = n * 3 + 1;
+    while (n > 1)
+    {
+        n = (n - 1) / 3;
+        for (i = 0; i < size - n; i++)
+        {
+            for (j = i + n; j > 0; j = j - n)
+            {
+                if (array[j] < array[j - n])
+                {
+                    temp = array[j];
+                    array[j] = array[j - n];
+                    array[j - n] = temp;
+                }
+            }
+        }
+        print_array(array, size);
+    }
+}
