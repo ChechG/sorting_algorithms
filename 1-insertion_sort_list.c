c1d192a2ecc0d65a379cb9f331bfcb6ede494928
ChechG <2100@holbertonschool.com>
2021-02-01 17:09:28 +0000

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *temp_n, *temp_sort, *swapper;
 	int len = dlistint_len(*list);
 
-	if (len < 2 || list == NULL || *list == NULL)
+	if (len < 2 || (*list)->next == NULL || *list == NULL)
 		return;
 	temp_n = *list;
 	if (len == 2)
