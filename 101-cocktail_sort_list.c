c106b22c0dd815915a1d55930e3b53b6e1093712
ChechG <2100@holbertonschool.com>
2021-02-02 00:32:09 +0000

    101

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -13,9 +13,10 @@ void cocktail_sort_list(listint_t **list)
 	if ((*list)->next == NULL)
 		return;
 	temp = *list;
+	recorro = *list;
 	while (temp != NULL)
 	{
-		recorro = *list;
+
 		while (recorro->next != NULL)
 		{
 			if (recorro->n > recorro->next->n)
