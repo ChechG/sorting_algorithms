e4485ff095db3e4b3cb27bd2a58bacbc33061fdf
ChechG <2100@holbertonschool.com>
2021-02-03 17:40:23 +0000

    104 sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,61 +1,73 @@
 #include "sort.h"
 
 /**
- * merge_sort - sorts an array of integers in ascending order
+ * heap_sort - sorts an array of integers in ascending order
  * @array: array to sort.
  * @size: size of array.
  */
 void heap_sort(int *array, size_t size)
 {
-    int n = (int)size, end, temp;
+	int n = (int)size, end, temp;
 
-    heapify(array, n);
-    end = n - 1;
-    while (end > 0)
-    {
-        temp = array[0];
-        array[0] = array[end];
-        array[end] = temp;
-        end = end - 1;
-        siftDown(array, 0, end, n);
-        print_array(array, n);
-    }
+	heapify(array, n);
+	end = n - 1;
+	while (end > 0)
+	{
+		temp = array[0];
+		array[0] = array[end];
+		array[end] = temp;
+		end = end - 1;
+		siftDown(array, 0, end, n);
+		print_array(array, n);
+	}
 }
 
+/**
+ * heapify - sorts an array of integers in ascending order
+ * @array: array to sort.
+ * @n: size of array.
+ */
 void heapify(int *array, int n)
 {
-    int start;
+	int start;
 
-    start = (n - 2) / 2;
-    while (start >= 0)
-    {
-        siftDown(array, start, n - 1, n);
-        start = start - 1;
-    }
+	start = (n - 2) / 2;
+	while (start >= 0)
+	{
+		siftDown(array, start, n - 1, n);
+		start = start - 1;
+	}
 }
 
+/**
+ * siftDown - sorts an array of integers in ascending order
+ * @array: array to sort.
+ * @start: start index of arrray.
+ * @end: end index of array.
+ * @n: size of array.
+ */
 void siftDown(int *array, int start, int end, int n)
 {
-    int root, child, temp = 0;
+	int root, child, temp = 0;
 
-    root = start;
-    while (root * 2 + 1 <= end)
-    {
-        child = root * 2 + 1;
-        if (child + 1 <= end && array[child] < array[child + 1])
-            child = child + 1;
-        if (array[root] < array[child])
-        {
-            temp = array[root];
-            array[root] = array[child];
-            array[child] = temp;
-            root = child;
-            print_array(array, n);
-        }
-        else
-        {
-            print_array(array, n);
-            return;
-        }
-    }
+	root = start;
+	while (root * 2 + 1 <= end)
+	{
+		child = root * 2 + 1;
+		if (child + 1 <= end && array[child] < array[child + 1])
+			child = child + 1;
+		if (array[root] < array[child])
+		{
+			temp = array[root];
+			array[root] = array[child];
+			array[child] = temp;
+			root = child;
+			print_array(array, n);
+		}
+		else
+		{
+			print_array(array, n);
+			return;
+		}
+	}
 }
